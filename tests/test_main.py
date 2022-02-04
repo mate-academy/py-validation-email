ff9c2fd0e0eef4a38da10c65ee1e4563dd5630b5
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-04 23:56:41 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,21 @@
 from app.main import validate_email
 
-# write your tests here
+
+def test_not_english_letters():
+    assert validate_email("емейл@mail.com") is False
+
+
+def test_not_allowed_characters():
+    assert validate_email("$mail@mail.com") is False
+
+
+def test_correct_symbols():
+    assert validate_email("AaBb3-8_.mail@mail.com") is True
+
+
+def test_dot_symbol_is_not_first():
+    assert validate_email(".alex@mail.com") is False
+
+
+def test_is_rate_sign_in_right_place():
+    assert validate_email("ma@il@mail.com") is False
