fadfc4a2a58fb46f667a7844b5afb1c80a78f8b9
Marko Khodan <khodanmarko@gmail.com>
2022-05-30 16:31:47 +0300

    request fixed

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,31 +4,26 @@ from app.main import validate_email
 class TestValidateEmail:
 
     def test_should_return_false_if_use_not_English(self):
-        assert not validate_email('Марко@mail.com')
+        assert validate_email('Марко@mail.com') is False
 
     def test_should_return_false_if_use_dots_as_first_or_last_char(self):
-        assert not validate_email('.test@mail.com')
-        assert not validate_email('test@mail.com.')
+        assert validate_email('.test@mail.com') is False
+        assert validate_email('test@mail.com.') is False
 
     def test_should_return_false_if_dots_come_one_after_the_other(self):
-        assert not validate_email('te..st@mail.com')  # function doesn't pass this assert
+        assert validate_email('te..st@mail.com') is False # function doesn't pass this assert
 
     def test_should_return_false_if_at_symbol_is_not_use(self):
-        assert not validate_email('testmail.com')
+        assert validate_email('testmail.com') is False
 
     def test_return_false_when_personal_info_or_domain_start_with_dot(self):
-        assert not validate_email('.test@mail.com')
-        assert not validate_email('test@.mail.com')  # function doesn't pass this assert
+        assert validate_email('.test@mail.com') is False
+        assert validate_email('test@.mail.com') is False # function doesn't pass this assert
 
     def test_should_return_false_if_use_not_allowed_symbol(self): # function doesn't pass this test
         not_allowed = "! $ % & ' * + / = ? ^ { | } ~"
         for char in not_allowed:
-            assert not validate_email(f'te{char}st@mail.com')
+            assert validate_email(f'te{char}st@mail.com') is False
 
     def test_return_false_if_double_dots_in_personal_info_part(self):
-        assert not validate_email('te:st@mail.com')
-
-
-
-
-
+        assert validate_email('te:st@mail.com') is False
