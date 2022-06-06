1793a57a09bd0e223513aa5bdc06551ff26d126d
Flashmobber <ufinaid@gmail.com>
2022-06-06 17:10:51 +0300

    All done.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,19 @@
+import pytest
 from app.main import validate_email
 
-# write your tests here
+
+@pytest.mark.parametrize("email, result",
+                         [("mail1@gmail.com", True),
+                          ("Мой-mail@gmail.com", False),
+                          ("mail№2@gmail.com", False),
+                          (".mail@gmail.com", False),
+                          ("mail-gmail.com", False),
+                          ("mail..@gmail.com", True),
+                          ("mail$@gmail.com", False),
+                          ("mail@gmail$.com", False),
+                          ("mail@gmailcom", False),
+                          ("mail@gmail-com", False),
+                          ("mail@gmail/com", False),
+                          ("mail@.gmail.com", True)])
+def test_validate_email(email: str, result: bool):
+    assert validate_email(email) == result
